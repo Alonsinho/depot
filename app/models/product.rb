3c3a7c7616965f1615de551ff8201f28bfa2f363
Alonsinho <antoal898@gmail.com>
2020-12-15 15:39:27 +0000

    Playtime Validation and Unit Testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
+  # Validates title length with personalised error message
+  validates :title, length: {
+    minimum: 10,
+    message: 'Must be at least 10 characters' }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
-    message: 'Must be a URL for GIF, JPG or PNG image.'
-  }
+    message: 'Must be a URL for GIF, JPG or PNG image.' }
 end
