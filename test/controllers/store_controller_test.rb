94256d4598aa24bcb4ad9451d6f333a505c01014
Alonsinho <antoal898@gmail.com>
2020-12-15 17:28:42 +0000

    Iteration C4: Functional Testing of Controllers

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,12 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    # Assertions based on products.yml fixture
+    assert_select 'nav.side_nav a', minimum: 4
+    assert_select 'main ul.catalog li', 3
+    assert_select 'h2', 'Clipper de Fresa'
+    # RegExp adapted to format 50.850,87€
+    assert_select '.price', /(\d+\.?),\d\d\€/
   end
 
 end
