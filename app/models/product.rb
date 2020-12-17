17c67e6ff28318ef79bb77cb1e92b2ad3f8f600d
Alonsinho <antoal898@gmail.com>
2020-12-17 17:00:59 +0000

    Iteration D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   # Validates title length with personalised error message
   validates :title, length: {
@@ -9,4 +13,14 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'Must be a URL for GIF, JPG or PNG image.' }
+
+  private
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
+
 end
