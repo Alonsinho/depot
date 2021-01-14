076de629eb2cac364a522c11573a30d26aee5f49
Alonsinho <antoal898@gmail.com>
2021-01-14 19:07:52 +0000

    Iteration E2: Handling Errors and E1 line items tests corrected

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -18,14 +18,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   test "should create line_item" do
     assert_difference('LineItem.count') do
       post line_items_url, params: {
-        product_id: products(:adidas).id
+        product_id: products(:clipper).id
       }
     end
 
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', '1 &times Adidas'
+    assert_select 'main li', "1 &times Clipper de Fresa"
   end
 
   test "should show line_item" do
@@ -39,7 +39,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item),
+    params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
